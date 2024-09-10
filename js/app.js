4699a6c03baeff0a2d7df14abecf3f7328323fa7
Patrique Porto <patrikrock@hotmail.com>
2024-09-10 11:38:39 -0300

    Testmonials slide

--- js/app.js
+++ js/app.js
@@ -39,3 +39,51 @@ window.addEventListener("scroll", () => {
    }
 })
 
+// Slick Slider Jquery
+
+$('.comentslick').slick({
+   dots: true,
+   Infinity: true,
+   slidesToShow: 5,
+   slidesToScroll: 2,
+   adaptativeHeight: true,
+   autoplay: true,
+   responsive: [
+      {
+         breakpoint: 1400,
+         settings: {
+            slidesToShow: 4,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+         }
+      },
+      {
+         breakpoint: 1024,
+         settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+         }
+      },
+      {
+         breakpoint: 800,
+         settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+           
+         }
+      },
+      {
+         breakpoint: 600,
+         settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+           
+         }
+      }
+
+   ]
+})
+
